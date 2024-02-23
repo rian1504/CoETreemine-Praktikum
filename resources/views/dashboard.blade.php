086394c4ae3a8c9e1c6eccc87cff7b726124339d
sarahalnr <isnainisarah061@gmail.com>
2024-02-23 10:58:39 +0700

    Fixing dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,7 +32,7 @@
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
             data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span clas\s="visually-hidden">Previous</span>
+            <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
             data-bs-slide="next">
