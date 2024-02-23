5ae0227b83f0579a2545c64828ccd2b8a0061de3
sarahalnr <isnainisarah061@gmail.com>
2024-02-23 10:55:55 +0700

    Fix dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,41 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
+    <title>{{$title}}</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 </head>
+
 <body>
-<div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+    <div id="carouselExampleIndicators" class="carousel slide" style="width: 1000px; margin: auto">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('/image/home.png') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('/image/home2.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('/image/home3.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span clas\s="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target=gi"#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<h1>Easier One-Stop Solutions for PCB & PCBA Services
 
-Simply upload designs to get instant quotes, place orders in minutes, and track orders in real-time<h1>
-    <p>PCB (Printed Circuit Board) adalah papan sirkuit yang digunakan untuk menyambungkan komponen elektronik pada suatu rangkaian. 
-        PCB digunakan pada berbagai perangkat elektronik, seperti komputer, telepon genggam, dan peralatan elektronik lainnya.
-         PCB dapat dibeli secara online melalui berbagai platform e-commerce seperti Tokopedia dan Shopee. Saat membeli PCB, pastikan untuk membaca deskripsi produk dengan teliti, karena terdapat berbagai jenis PCB dengan spesifikasi yang berbeda-beda. 
-         Beberapa deskripsi produk PCB yang dapat ditemukan di platform e-commerce meliputi jenis bahan, ukuran, dan warna PCB, serta komponen yang dibutuhkan untuk memasang PCB tersebut. Selain itu, terdapat juga inovasi terbaru dalam pembuatan PCB, seperti PCB Desktop Printer yang memungkinkan pencetakan langsung jalur rangkaian elektronik pada papan sirkuit menggunakan tinta berbasis minyak (solvent based ink)<p>
-
-
+    <h1>Easier One-Stop Solutions for PCB & PCBA Services Simply upload designs to get instant quotes, place orders in minutes, and track orders in real-time</h1>
+            <p>PCB (Printed Circuit Board) adalah papan sirkuit yang digunakan untuk menyambungkan komponen
+                elektronik
+                pada suatu rangkaian.
+                PCB digunakan pada berbagai perangkat elektronik, seperti komputer, telepon genggam, dan peralatan
+                elektronik lainnya.
+                PCB dapat dibeli secara online melalui berbagai platform e-commerce seperti Tokopedia dan Shopee.
+                Saat
+                membeli PCB, pastikan untuk membaca deskripsi produk dengan teliti, karena terdapat berbagai jenis
+                PCB
+                dengan spesifikasi yang berbeda-beda.
+                Beberapa deskripsi produk PCB yang dapat ditemukan di platform e-commerce meliputi jenis bahan,
+                ukuran,
+                dan warna PCB, serta komponen yang dibutuhkan untuk memasang PCB tersebut. Selain itu, terdapat juga
+                inovasi terbaru dalam pembuatan PCB, seperti PCB Desktop Printer yang memungkinkan pencetakan
+                langsung
+                jalur rangkaian elektronik pada papan sirkuit menggunakan tinta berbasis minyak (solvent based ink)
+                <p>
+                  <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
