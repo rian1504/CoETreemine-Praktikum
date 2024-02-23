9caad629d1fe46ff85be1b084a94f9af5a037a09
reezqiii <velasyifa02@gmail.com>
2024-02-23 10:33:10 +0700

    Fix bootstrap

--- resources/views/list_item.blade.php
+++ resources/views/list_item.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="bootstrap/css">
 </head>
 <body>
-    <h1>LIST ITEM</h1>
-    <p>{{ $nama_barang }}</p>
+    
 </body>
 </html>
