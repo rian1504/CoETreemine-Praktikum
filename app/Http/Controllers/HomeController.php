5d7a15336abb88505914d825f66f15e5bbd5f9e8
reezqiii <velasyifa02@gmail.com>
2024-02-23 11:24:09 +0700

    home anjay

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
         // ];
         // return view('home')->with($data);
 
-        $nama = "Doraemon";
+        $nama = "Nobita";
         $pekerjaan = "Student";
         return view ('home', compact('nama','pekerjaan'));
     }
