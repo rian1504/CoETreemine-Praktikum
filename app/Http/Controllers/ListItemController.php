f89a3431a2796d05c74d1618461955c99adff107
reezqiii <velasyifa02@gmail.com>
2024-03-01 09:42:09 +0700

    Praktikum 4

--- app/Http/Controllers/ListItemController.php
+++ app/Http/Controllers/ListItemController.php
@@ -8,13 +8,16 @@ class ListItemController extends Controller
 {
     function index()
     {
-        return view("list_item", [
-            "title" => "List Item",
-            "barang" => [
-                "barang1" => "PCB Prototype",
-                "barang2" => "PCB Assembly",
-                "barang3" => "PCB Assembly"
-            ]
-        ]);
+        $dataBarang = [
+            ["id" => 1, "nama_barang" => "Microcontroller Boards", "gambar" => "micro.gif"],
+            ["id" => 2, "nama_barang" => "Industrial Arduino Shields", "gambar" => "arduino.jpg"],
+            ["id" => 3, "nama_barang" => "Motor Control", "gambar" => "motor.jpg"],
+            ["id" => 4, "nama_barang" => "Breakout Boards", "gambar" => "breakout.jpg"],
+            ["id" => 5, "nama_barang" => "Upgrades and Components", "gambar" => "upgrades.png"],
+            ["id" => 6, "nama_barang" => "Serial Boards", "gambar" => "serial.png"],
+            ["id" => 7, "nama_barang" => "Memory", "gambar" => "memory.jpg"],
+        ];
+
+        return view("list_item", compact("dataBarang"));
     }
 }
--- resources/views/list_item.blade.php
+++ resources/views/list_item.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title }}</title>
+    <title>LIST ITEM</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 
@@ -13,25 +13,14 @@
     <h1 class="text-center">LIST ITEM BARANG</h1>
     <div class="container mt-3">
         <div class="row justify-content-center">
-            <div class="card" style="width: 18rem;">
-                <img src="image/pcb1.jpg" class="card-img-top" alt="...">
+            @foreach($dataBarang as $data)
+            <div class="card mx-2 mt-2" style="width: 18rem;">
+                <img src="image/portofolio/{{ $data['gambar'] }}" class="card-img-top" alt="..." height="200">
                 <div class="card-body">
-                    <p class="card-text">{{ $barang["barang1"] }}</p>
+                    <p class="card-text">{{ $data['nama_barang'] }}</p>
                 </div>
             </div>
-            <div class="card" style="width: 18rem; margin: 0 50px;">
-                <img src="image/pcb4.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">{{ $barang["barang2"] }}</p>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="image/pcb3.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">{{ $barang["barang3"] }}</p>
-                </div>
-            </div>
-            
+            @endforeach
         </div>
     </div>
 </body>
