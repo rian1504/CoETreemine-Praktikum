d205c330be60935d5c8e2994c3cd4c13c75ccf2f
reezqiii <velasyifa02@gmail.com>
2024-03-15 09:18:10 +0700

    vela code

--- app/Http/Controllers/VelaController.php
+++ app/Http/Controllers/VelaController.php
@@ -9,6 +9,6 @@ class VelaController extends Controller
     function pagi(){
         $nama = "Sarah";
         $kelas = "Informatika";
-        return view ("vela_view~" , compact("nama" , "kelas"));
+        return view ("vela_view" , compact("nama" , "kelas"));
     }
 }
